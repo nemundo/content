171c971887842187b3804a7ed34ab3bcdb5df5f3
Lang Urs <urs.lang@luzern.com>
2024-01-30 22:00:36 +0100

    .

---
+++ src/Usergroup/ContentUsergroup.php
@@ -0,0 +1,9 @@
+<?php
+namespace Nemundo\Content\Usergroup;
+use Nemundo\User\Usergroup\AbstractUsergroup;
+class ContentUsergroup extends AbstractUsergroup {
+protected function loadUsergroup() {
+$this->usergroup = 'Content';
+$this->usergroupId = '5fc582f4-ee5d-4210-85b8-10d3f88c8cc3';
+}
+}
